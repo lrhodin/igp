11c533dd594bbaa9547fada697373b13d3f5d068
Ludvig Rhodin <lrhodin@gmail.com>
2013-11-07 03:44:23 -0700

    manual adding of videos working

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Igp::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'pages#library'
+  root 'pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
