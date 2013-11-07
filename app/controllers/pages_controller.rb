6e47dbf6778429c9f64c9b0976f59594010b3bca
Ludvig Rhodin <lrhodin@gmail.com>
2013-11-07 12:42:35 -0700

    links to albums added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,9 @@
 class PagesController < ApplicationController
   def library
+    @albums = Video.uniq.pluck(:album)
     if params[:album]
       @videos = Video.where(Album: params[:album])
+      @album = params[:album]
     else
       @videos = Video.all
     end
