75da0a9e68332be35621ca7102658061c607f2b8
James Adson <james.adson@gmail.com>
2014-06-20 16:56:03 -0600

    Started working on controller for homepage. Made changes to layout of video admin page.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,23 @@
 class PagesController < ApplicationController
 
+  def home
+    @videos = Video.all
+    @vidlist = Array.new
+    @videos.each do |video|
+      @vidlist.push(video.vid)
+    end
+
+    @vidlist.sort!
+    @vidlist.reverse! #list of Vimeo IDs from highest (most recent) to lowest
+    @vidlist = @vidlist[0...9] #9 highest Vimeo IDs ==> 9 most recent videos
+  end
+
+
+
+
+
+
+
   # get pages/library
   def library
     @albums = Video.uniq.pluck(:album)
@@ -27,6 +45,15 @@ class PagesController < ApplicationController
       format.html { redirect_to "pages/viewer" }
       format.js
     end
+  end
+
+  #putting in static pages just for clarity's sake
+  def supporters
+    render 'supporters'
+  end
+
+  def staff
+    render 'staff'
   end
 
 end
