6e6f3261d295cc356ec445bc6cbf4f452f48979d
James Adson <james.adson@gmail.com>
2014-06-23 15:49:51 -0600

    more updates

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+//= require jquery.tools.min
 
 jQuery.ajaxSetup({
   'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
