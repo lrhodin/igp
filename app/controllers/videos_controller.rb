2536af42c97d3409317606cc1ab4b43be1b1af3e
Ludvig Rhodin <lrhodin@gmail.com>
2014-08-20 14:29:44 -0600

    fixed albums->videos routing issue

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -42,7 +42,7 @@ class VideosController < ApplicationController
 
     respond_to do |format|
       if @video.save
-        format.html { redirect_to videos_url, notice: 'Video was successfully created.' }
+        format.html { redirect_to albums_url, notice: 'Video was successfully created.' }
         format.json { render action: 'show', status: :created, location: @video }
       else
         format.html { render action: 'new' }
@@ -56,7 +56,7 @@ class VideosController < ApplicationController
   def update
     respond_to do |format|
       if @video.update(video_params)
-        format.html { redirect_to videos_url, notice: 'Video was successfully updated.' }
+        format.html { redirect_to albums_url, notice: 'Video was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
@@ -70,7 +70,7 @@ class VideosController < ApplicationController
   def destroy
     @video.destroy
     respond_to do |format|
-      format.html { redirect_to videos_url }
+      format.html { redirect_to albums_url }
       format.json { head :no_content }
     end
   end
